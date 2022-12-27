c76a55bb6c6a582f801c5858f49d42a85600f687
rkbennett <44292326+rkbennett@users.noreply.github.com>
2022-12-27 12:52:51 -0600

    adds myimporter.py

---
+++ myimporter.py
@@ -0,0 +1,21 @@
+import sys, types
+
+class MyImplorter(object):
+    def __init__(self, override):
+        self.override = override
+  
+    def find_module(self, module, path=None):
+        if self.override:
+            return self
+        else:
+            return None
+      
+    def load_module(self, name):
+        mod = types.ModuleType(name)
+        mod.__loader__ = self
+        mod.__file__ = '/foo/bar/baz'
+        mod.__package__ = name
+        mod.__path__ = name.replace('.','/') + '/'
+        sys.modules[name] = mod
+        exec('print("Meow")\n',mod.__dict__)
+        return mod
